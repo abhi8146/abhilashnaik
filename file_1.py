9f1bb57f165448d176992f4ea9f741e851164ae7
abhi8146 <162983134+abhi8146@users.noreply.github.com>
2024-03-11 16:04:18 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,24 @@
+# print has 5 parameters:
+# object --> the valve to be printed is object 
+# sep --> seperator
+# Eg:
+     # a = 8
+     # b = 0
+     # c = 89 
+# print(a,b,c,  sep='$')
+
+# end --> which is used to add the string at the end of the valve,
+# # it will not keep the command to stay in same line
+ # Eg: 
+    # print(str(a),b, end='%%')
+    # print(str(c))
+
+# ! -----> variables
+#? variable is a name which is used to identify valve of variable
+#? which is stored in a specific memory location
+
+# ! Rules to dedfine variables
+# 1.) variable have to start with only alphabets, underscores
+# 2.) the char following the 1st char can be alphabets, numbers, unscore
+# 3.) no space are allowed 
+# 4.) no special characters 
