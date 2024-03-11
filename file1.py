cd40203cf354f85e6f9cc83906aa1a23a0246505
abhi8146 <162983134+abhi8146@users.noreply.github.com>
2024-03-11 16:04:34 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,36 @@
+#print("hello")
+#print("hello world")
+"""
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+"""
+print("hello world")
+"""
+1.)commenting is used for making documentation in a python file
+2.)when the line is connected, that line will not be considerd
+as a part of the code.
+3.)when the program is executed, connected line will not be in the part
+of execution.
+           Eg:
+               #print("hello 1")
+
+multiple line commenting can be done using code.the line betweenthe
+quotes will be commented.
+            Eg:
+
+            """
+            print("hello world")
+
+            print("hello world")
+
+            print("hello wprld")
+            """
+print("hello world")
+,,,
+import (this)
+
+
+controle and data structures
